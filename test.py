98c9443b3af78b58dd98583db80ed72742289e4c
lglkh123 <72617811+lglkh123@users.noreply.github.com>
2020-10-11 20:03:31 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ x = int(input("x좌표를 입력하세요 = "))
 y = int(input("y좌표를 입력하세요 = "))
 
 if  (x>0)and(y>0) :
-    print("제1사분면입니다.")
+    print("제일사분면입니다.")
 elif (x<0)and(y>0) :
-    print("제2사분면입니다.")
+    print("제이사분면입니다.")
 elif (x<0)and(y<0) :
-    print("제3사분면입니다.")
+    print("제삼사분면입니다.")
 else :
-    print("제4사분면입니다.")
+    print("제사사분면입니다.")
